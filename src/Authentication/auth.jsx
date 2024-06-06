e1ac8332fcf0304a1673849d1a7ea5a73b79d10f
asobosamuel <samuelasobo04@gmail.com>
2024-06-06 11:10:34 +0100

    minor changes

--- src/Authentication/auth.jsx
+++ src/Authentication/auth.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { useContext } from "react";
-import { useState } from "react";
 
 const AuthContext = React.createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  // const [getUser, setUser] = useState({});
-
   const getUser = () => {
     const userString = sessionStorage.getItem("user");
     const user = JSON.parse(userString);
