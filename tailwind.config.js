ef3816424e9f955bcdee402e592572af0bc5931e
spykelionel <social.lionel@gmail.com>
2024-06-13 09:32:24 +0100

    config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    // colors: {
-    //   primary: "#d6bbfb",
-    //   secondary: "#e9d7fe",
-    //   light: "#f4ebff",
-    //   dark: "#6941c6",
-    // },
   },
   plugins: [],
 };
